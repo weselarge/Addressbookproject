764f24957f51d83068ffc02a1613bd8af90f0cf0
teminelkan <50968629+teminelkan@users.noreply.github.com>
2020-12-17 19:47:45 -0600

    Update ContactForm.java
    Made changes per jira

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -26,8 +26,8 @@ public class ContactForm extends FormLayout {
     Button save = new Button("Save", this::save);
     Button cancel = new Button("Cancel", this::cancel);
     TextField firstName = new TextField("First Name");
-    TextField lastName = new TextField("Surname");
-    TextField phone = new TextField("Telephone");
+    TextField lastName = new TextField("Last Name");
+    TextField phone = new TextField("Phone Number");
     TextField email = new TextField("Email");
     DateField birthDate = new DateField("Birth date");
 
