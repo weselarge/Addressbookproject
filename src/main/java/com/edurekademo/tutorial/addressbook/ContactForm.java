fcb22847383ad086adcf777ec569c4457900abf2
teminelkan <50968629+teminelkan@users.noreply.github.com>
2021-05-04 11:56:39 -0500

    Update ContactForm.java
    changed to last name

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -26,7 +26,7 @@ public class ContactForm extends FormLayout {
     Button save = new Button("Save", this::save);
     Button cancel = new Button("Cancel", this::cancel);
     TextField firstName = new TextField("First Name");
-    TextField lastName = new TextField("Surname");
+    TextField lastName = new TextField("Last Name");
     TextField phone = new TextField("Phone Num");
     TextField email = new TextField("Email");
     DateField birthDate = new DateField("Birth date");
